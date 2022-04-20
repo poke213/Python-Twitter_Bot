163b02db4b54c2ccbbebd0fab35c5f7e689e3bcf
Leon <leonlam34@gmail.com>
2022-04-19 20:57:46 -0500

    -

--- ML/HandWrittenDigitProject/HandReco.py
+++ ML/HandWrittenDigitProject/HandReco.py
@@ -1,2 +1,10 @@
 import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+import tensorflow as tf
+
+
 
