b637d50538097359ae7b0eb7867034e5f4b7962a
Leon-Lam919 <leonlam34@gmail.com>
2023-05-13 11:48:54 -0500

    fixed messages, bot works now

--- Tutorial/Bot.py
+++ Tutorial/Bot.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 #create bot
 
 # imports discord lib and dotenv lib
@@ -6,15 +7,32 @@ import os
 import discord
 from dotenv import load_dotenv
 
-load_dotenv()
+intents = discord.Intents.default()
+intents.message_content = True
+
 
 # calls DISCORD_TOKEN to get discord token from .env file
+load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client(intents=discord.Intents.default())
 
+client = discord.Client(intents=intents)
+
+#lets discord server know bot has entered server
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord')
+    
+# reads user input and will outputt hello
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    
+    if message.content.startswith('$butt'):
+        await message.channel.send('butt hehe')
     
 client.run(TOKEN)
