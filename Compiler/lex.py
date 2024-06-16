5465200bc3b4731173edf42ba74b7a1d2e5898ce
Leon Lam <leonlam34@gmail.com>
2024-06-16 11:17:42 -0500

    added to lex

--- Compiler/lex.py
+++ Compiler/lex.py
@@ -1,11 +1,47 @@
-class token:
+import enum
+import sys
+
+#class for Token
+class Token:   
     def __init__(self, tokenText, tokenKind):
         self.text = tokenText
         self.kind = tokenKind
-        
+
+#enum for token types
+class tokenType(enum.Enum):
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    INDENT = 2
+    STRING = 3
+    #keywords
+    LABEL = 101
+    GOTO = 102
+    PRINT = 103
+    INPUT = 104
+    LET = 105
+    IF = 106
+    THEN = 107
+    ENDIF = 108
+    WHILE = 109
+    REPEAT = 110
+    ENDHWILE = 111
+    #operators
+    EQ = 201
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 210
+    GTEQ = 211
 
 
-class lex:
+#Lexer class
+class Lexer:
     def __init__(self, source):
         #adds 
         self.source = source + '\n' #adds newline to the end of the source code
@@ -27,11 +63,12 @@ class lex:
     
     #Invalid token found, print error message and exit
     def abort(self, message):
-        pass
+        sys.exit("Lexing error: " + message)
     
     #skip whitespace
     def skipWhitespace(self):
-        pass
+        while self.curChar == ' ' or self.curChar == '\t' or self.curChar == '\r':
+            self.nextChar()
     
     #skip comments
     def skipComment(self):
@@ -39,20 +76,25 @@ class lex:
     
     #return token
     def getToken(self):
+        token = None
+        
+        self.skipWhitespace()
+        
         #checks for each token
         if self.curChar == '+':
-            pass
+            token = Token(self.curChar, tokenType.PLUS)
         elif self.curChar == '-':
-            pass
+            token = Token(self.curChar, tokenType.MINUS)
         elif self.curChar == '*':
-            pass
+            token = Token(self.curChar, tokenType.ASTERISK)
         elif self.curChar == '/':
-            pass
+            token = Token(self.curChar, tokenType.SLASH)
         elif self.curChar == '\0':
-            pass
+            token = Token(self.curChar, tokenType.EOF)
         elif self.curChar == '\n':
-            pass
+            token = Token(self.curChar, tokenType.NEWLINE)
         else:
-            pass
+            self.abort("Unknown token: " + self.curChar)
         
         self.nextChar()
+        return token
--- Compiler/test.py
+++ Compiler/test.py
@@ -1,11 +1,12 @@
 from lex import *
 
 def main():
-    source = "LET x = 5"
-    l = lex(source)
-    
-    while l.curChar != '\0':
-        print(l.curChar)
-        l.nextChar()
-        
+    source = "+- */"
+    lexer = Lexer(source)
+
+    token = lexer.getToken()
+    while token is not None and token.kind != tokenType.EOF:
+        print(token.kind)
+        token = lexer.getToken()
+
 main()
